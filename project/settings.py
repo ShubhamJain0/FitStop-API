8a3212a2cee4cb78712c6f9119a9778b7605bc8c
ShubhamJain0 <kotechashubham94@gmail.com>
2021-04-19 18:24:20 +0530

    Config for serving static files via nginx

--- project/settings.py
+++ project/settings.py
@@ -138,6 +138,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/staticfiles/'
 
 
 MEDIA_URL = '/media/'
--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('store/', include('store.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
